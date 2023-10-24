5f27118da7f0592e70f7e077b857bf4fe01e82e0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-24 15:45:38 +0800

    Trigger radio buttons

--- app/src/main/java/com/example/simplemobileapp/CheckboxActivity.java
+++ app/src/main/java/com/example/simplemobileapp/CheckboxActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class CheckboxActivity extends AppCompatActivity {
 
     private CheckBox check1;
     private Button button;
+    private RadioGroup radioGroup;
+    private RadioButton radioBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,12 @@ public class CheckboxActivity extends AppCompatActivity {
 
         check1 = findViewById(R.id.checkBox);
         button = findViewById(R.id.button);
+        radioGroup = findViewById(R.id.radioGroup);
 
         check1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(((CheckBox)v).isChecked()){
+                if (((CheckBox) v).isChecked()) {
                     Toast.makeText(CheckboxActivity.this, "Red is selected", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -33,9 +38,15 @@ public class CheckboxActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StringBuffer result = new StringBuffer();
-                result.append("Red is ").append(check1.isChecked());
-                Toast.makeText(CheckboxActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
+
+                // StringBuffer result = new StringBuffer();
+                // result.append("Red is ").append(check1.isChecked());
+                // Toast.makeText(CheckboxActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
+
+                int selected_id = radioGroup.getCheckedRadioButtonId();
+                radioBtn = findViewById(selected_id);
+                Toast.makeText(CheckboxActivity.this, radioBtn.getText().toString(), Toast.LENGTH_SHORT).show();
+
             }
         });
 
