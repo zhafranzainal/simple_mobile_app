fc7b0d28e05933d6bb52045a25ec2b284005a640
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-30 19:50:22 +0800

    Manipulate text size

--- app/src/main/java/com/example/simplemobileapp/TextManipulationActivity.java
+++ app/src/main/java/com/example/simplemobileapp/TextManipulationActivity.java
@@ -13,7 +13,10 @@ public class TextManipulationActivity extends AppCompatActivity {
     private TextView text;
 
     private RadioGroup radGroupColour;
-    private RadioButton radbtnBlack, radbtnRed, radbtnBlue;
+    private RadioButton radBtnBlack, radBtnRed, radBtnBlue;
+
+    private RadioGroup radGroupSize;
+    private RadioButton radBtnSmall, radBtnMed, radBtnLarge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +25,42 @@ public class TextManipulationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text_manipulation);
 
         text = findViewById(R.id.textView);
+
         radGroupColour = findViewById(R.id.radioGroupColour);
-        radbtnBlack = findViewById(R.id.radioButtonBlack);
-        radbtnRed = findViewById(R.id.radioButtonRed);
-        radbtnBlue = findViewById(R.id.radioButtonBlue);
+        radBtnBlack = findViewById(R.id.radioButtonBlack);
+        radBtnRed = findViewById(R.id.radioButtonRed);
+        radBtnBlue = findViewById(R.id.radioButtonBlue);
+
+        radGroupSize = findViewById(R.id.radioGroupSize);
+        radBtnSmall = findViewById(R.id.radioButton18);
+        radBtnMed = findViewById(R.id.radioButton24);
+        radBtnLarge = findViewById(R.id.radioButton28);
 
         radGroupColour.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == radbtnBlack.getId()) {
+                if (checkedId == radBtnBlack.getId()) {
                     text.setTextColor(Color.BLACK);
-                } else if (checkedId == radbtnRed.getId()) {
+                } else if (checkedId == radBtnRed.getId()) {
                     text.setTextColor(Color.RED);
-                } else if (checkedId == radbtnBlue.getId()) {
+                } else if (checkedId == radBtnBlue.getId()) {
                     text.setTextColor(Color.BLUE);
+                }
+            }
+
+        });
+
+        radGroupSize.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == radBtnSmall.getId()) {
+                    text.setTextSize(18);
+                } else if (checkedId == radBtnMed.getId()) {
+                    text.setTextSize(24);
+                } else if (checkedId == radBtnLarge.getId()) {
+                    text.setTextSize(28);
                 }
             }
 
