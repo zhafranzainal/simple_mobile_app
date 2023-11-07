913b7052d264fc26c4a74ddfe68d488159c0555c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-07 15:27:08 +0800

    Grant call phone permission

--- app/src/main/java/com/example/simplemobileapp/MyIntentActivity.java
+++ app/src/main/java/com/example/simplemobileapp/MyIntentActivity.java
@@ -1,18 +1,25 @@
 package com.example.simplemobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MyIntentActivity extends AppCompatActivity {
 
     private Button homeBtn;
     private Button callBtn;
     private Button smsBtn;
+
+    private final String mCallPhonePermission = android.Manifest.permission.CALL_PHONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +41,11 @@ public class MyIntentActivity extends AppCompatActivity {
         callBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:60172923450"));
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(MyIntentActivity.this, mCallPhonePermission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MyIntentActivity.this, new String[]{mCallPhonePermission}, 1);
+                } else {
+                    initiatePhoneCall();
+                }
             }
         });
 
@@ -49,5 +58,23 @@ public class MyIntentActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initiatePhoneCall();
+            } else {
+                Toast.makeText(this, "Permission to make a phone call is required.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void initiatePhoneCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:60172923450"));
+        startActivity(intent);
     }
 }
