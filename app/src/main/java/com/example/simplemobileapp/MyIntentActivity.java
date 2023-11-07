a772c8f280a1df091e19d4f8269e1c1a8629d888
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-07 15:10:25 +0800

    Add onClickListener for each button

--- app/src/main/java/com/example/simplemobileapp/MyIntentActivity.java
+++ app/src/main/java/com/example/simplemobileapp/MyIntentActivity.java
@@ -2,13 +2,52 @@ package com.example.simplemobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyIntentActivity extends AppCompatActivity {
+
+    private Button homeBtn;
+    private Button callBtn;
+    private Button smsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_intent);
+
+        homeBtn = findViewById(R.id.homeButton);
+        callBtn = findViewById(R.id.callButton);
+        smsBtn = findViewById(R.id.smsButton);
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                startActivity(intent);
+            }
+        });
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:60172923450"));
+                startActivity(intent);
+            }
+        });
+
+        smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("smsto:60172923450"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
