4ff66961e4e2dd0c308d0ee7ab9662a497bb2894
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-28 15:11:20 +0800

    Write to Firebase

--- app/src/main/java/com/example/simplemobileapp/MyFirebaseActivity.java
+++ app/src/main/java/com/example/simplemobileapp/MyFirebaseActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MyFirebaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_firebase);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
     }
 }
