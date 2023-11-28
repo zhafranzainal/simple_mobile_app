f85dce44f6f8e1d620cb8e9b8479ebd01c854b89
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-28 15:22:21 +0800

    Add component in Java

--- app/src/main/java/com/example/simplemobileapp/MyFirebaseActivity.java
+++ app/src/main/java/com/example/simplemobileapp/MyFirebaseActivity.java
@@ -3,17 +3,29 @@ package com.example.simplemobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MyFirebaseActivity extends AppCompatActivity {
 
+    private EditText itemName;
+    private EditText itemPrice;
+    private EditText itemQty;
+    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_firebase);
+
+        itemName = findViewById(R.id.editTextItem);
+        itemPrice = findViewById(R.id.editTextPrice);
+        itemQty = findViewById(R.id.editTextQuantity);
+        btn = findViewById(R.id.buttonSubmit);
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
